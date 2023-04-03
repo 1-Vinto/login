ba4370ecd3536db7c8870e80aa2101977943db43
1-Vinto <112655268+1-Vinto@users.noreply.github.com>
2023-04-03 14:23:58 -0300

    Commit full APP

---
+++ Tela-Login/src/Routes/routes.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Login from "../Pages/Login";
+import ProtectedRoutes from "./ProtectedRoutes"
+//import Cadastro from '../Components/Cadastro/index'
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const Routering = () => {
+    return ( 
+    <Router>
+        <Routes>
+            <Route path="*" element={<Login/>}/>
+            {/* <Route path="/cadastrar" element={<Cadastro/>}/> */}
+            <Route path="/home" element={
+                <ProtectedRoutes>
+                <h1>Tela Home</h1>
+                </ProtectedRoutes>
+            }/>
+        </Routes>
+    </Router>
+     );
+}
+ 
+export default Routering;
